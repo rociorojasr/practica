e096366b4f9d59ad4c589f3ed2527bed7f2fe890
rociorojasr <117869256+rociorojasr@users.noreply.github.com>
2022-12-08 20:33:49 -0500

    Update tablero2.py

--- tablero2.py
+++ tablero2.py
@@ -6,17 +6,24 @@ from pyecharts import options as opts
 from pyecharts.charts import Bar
 from streamlit_echarts import st_pyecharts
 from streamlit_echarts import st_echarts
-
+##########################################################################################
 #id = 1Gu65mnJ_lxE0BdbkL1nTq5qaFJ1dJ9tq
-@st.experimental_memo
-def download_data():
-     url = "https://docs.google.com/spreadsheets/d/13iNig4VIvt5Gm0znUt2eq3_YnGCgCQHM/edit#gid=46563781"
-     output = 'data.csv'
-     gdown.download(url,output,quiet=False)
+#@st.experimental_memo
+#def download_data():
+#    url = "https://docs.google.com/spreadsheets/d/13iNig4VIvt5Gm0znUt2eq3_YnGCgCQHM/edit#gid=46563781"
+#     output = 'data.csv'
+###############################################################################################
+#id=13iNig4VIvt5Gm0znUt2eq3_YnGCgCQHM
+@st.experimental_nemo
+def dowload_data():
+	#https://drive.google.com/uc?id=MIOOO
+	url="https://drive.google.com/uc?id=13iNig4VIvt5Gm0znUt2eq3_YnGCgCQHM"
+	output="data.csv"
+	gdown.download(url,output,quiet=False)
 download_data()
 #df = pd.read_csv(r'C:\Users\51952\Downloads\PositivosCovid\positivos_covid.csv',sep=";", skip_blank_lines=True, parse_dates=['id_centro_vacunacion', 'id_eess'])
-
-df = pd.read_csv('data.csv',sep = ";",  skip_blank_lines=True, nrows=1000000,parse_dates=['id_centro_vacunacion', 'id_eess'])
+df = pd.read_csv('data.csv',sep = ";",  skip_blank_lines=True, nrows=1000,parse_dates=['id_centro_vacunacion', 'id_eess'])
+st.dataframe(data.head(20))
 
 st.title("      CASOS POSITIVOS: COVID-19", anchor = None )
 from PIL import Image
